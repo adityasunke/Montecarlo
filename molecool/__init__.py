25551efde4918797d40a32b5438908fe9d331836
Aditya Sunke <adityasunke2004@vt.edu>
2025-05-06 14:08:23 -0400

    Changes to init

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,10 +1,10 @@
 """The montecarlo package for PHYS3684"""
 
 # Add imports here
-#from montecarlo import montecarlo
-#from .bitstring import *
-#from .isinghamiltonian import *
-#from .metropolis import *
+from montecarlo import montecarlo
+from .bitstring import *
+from .ising import *
+from .metropolis import *
 
 from .montecarlo import *
 from ._version import __version__
