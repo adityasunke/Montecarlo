b4cbeb1ecb32e2c0186a2033ee530d6d48f62865
Aditya Sunke <adityasunke2004@vt.edu>
2025-05-06 22:58:30 -0400

    Testing the BitString class

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -13,21 +13,3 @@ import molecool
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "molecool" in sys.modules
-
-import pytest
-from molecool.bitstring import BitString
-
-def test_init():
-    bs = BitString(5)
-    assert len(bs.config) == 5
-
-def test_flip_site():
-    bs = BitString(3)
-    bs.flip_site(1)
-    assert bs.config[1] == 1
-
-def test_on_off_counts():
-    bs = BitString(2)
-    bs.flip_site(0)
-    assert bs.on() == 1
-    assert bs.off() == 1
