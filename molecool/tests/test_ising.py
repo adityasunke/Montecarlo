0ebae0f146c782cc116836c1ea9a87ebb69312f5
Aditya Sunke <adityasunke2004@vt.edu>
2025-05-07 18:04:41 -0400

    Deleted the bitstring testing and added testing for ising

--- molecool/tests/test_ising.py
+++ molecool/tests/test_ising.py
@@ -1,19 +1,3 @@
-"""
-Unit and regression test for the molecool package.
-"""
-
-# Import package, test suite, and other packages as needed
-import sys
-
-import pytest
-
-import molecool
-
-
-def test_molecool_imported():
-    """Sample test, will always pass so long as import statement worked."""
-    assert "molecool" in sys.modules
-
 import pytest
 import numpy as np
 import networkx as nx
